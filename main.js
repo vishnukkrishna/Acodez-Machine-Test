ff51b02c3aed815b9729976d7ef3f18926c21516
vishnukkrishna <vishnukkrishna1999@gmail.com>
2024-10-04 11:52:22 +0530

    feat: Add popular courses section with horizontal scrolling functionality
    - Implemented popular courses section with scrollable course cards.
    - Added 'moveLeft' and 'moveRight' buttons for horizontal scrolling interaction.
    - Integrated JavaScript for smooth horizontal scrolling.
    - Styled course cards with flexbox and responsive design for various screen sizes.
    - Used media queries to adjust layout on tablets and mobile devices, ensuring optimal display across devices.
    - Added hover effects and button transitions for better user experience.

--- main.js
+++ main.js
@@ -2,3 +2,28 @@ function toggleMenu() {
   const navLinks = document.querySelector(".nav-links");
   navLinks.classList.toggle("active");
 }
+
+let scrollPosition = 0;
+document.getElementById("moveLeft").addEventListener("click", function () {
+  const container = document.getElementById("course-scroll");
+  if (container) {
+    const newScrollPosition = scrollPosition - container.offsetWidth * 0.4;
+    scrollPosition = newScrollPosition;
+    container.scrollTo({
+      left: newScrollPosition,
+      behavior: "smooth",
+    });
+  }
+});
+
+document.getElementById("moveRight").addEventListener("click", function () {
+  const container = document.getElementById("course-scroll");
+  if (container) {
+    const newScrollPosition = scrollPosition + container.offsetWidth * 0.4;
+    scrollPosition = newScrollPosition;
+    container.scrollTo({
+      left: newScrollPosition,
+      behavior: "smooth",
+    });
+  }
+});
